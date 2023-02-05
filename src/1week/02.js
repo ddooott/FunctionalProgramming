e1078a9227451b4f2290f0e9cb44631a55f0cc1d
Chaerin Lee <124489210+ddooott@users.noreply.github.com>
2023-02-05 18:58:16 +0900

    Update 02.js

--- src/1week/02.js
+++ src/1week/02.js
@@ -9,3 +9,18 @@ function accumulate(arr) {
 }
 
 exports.accumulate = accumulate;
+
+// 따라 적기연습 //
+
+
+function accumulate(arr) {
+  let accumulator = 0;
+  
+  for (let i =0 ; i< arr.length; i++) {
+    accumulator += arr[i];
+  }
+  
+  return accumulator;
+}
+
+exports.accumulate = accumulate;
