6a0a1b9e9401f5d3a4733bed29c81c60b8b146a3
Chaerin Lee <124489210+ddooott@users.noreply.github.com>
2023-02-05 19:14:03 +0900

    Update 03.js

--- src/1week/03.js
+++ src/1week/03.js
@@ -13,3 +13,18 @@ function multiDimensionalAccumulate(multiDimensionalArr) {
 }
 
 exports.multiDimensionalAccumulate = multiDimensionalAccumulate;
+
+// 코드 따라적기 //
+function multiDimensionalAccumulate(multiDimensionalArr) {
+  let accumulator = 0;
+  
+  for (let i = 0; i< multiDimensionalArr.lengh; i++) {
+    for (let j = 0; j < multiDimensionalArr[i].length; j++) {
+      if (j < i) {
+        accumulator += multiDimensionalArr[i][j];
+      }
+    }
+  }
+  
+  return accumulator;
+}
